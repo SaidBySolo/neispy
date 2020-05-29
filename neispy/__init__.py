0a56190711387f25db9f136e0b3a8e349a414666
SaidBySolo <saidbysolo@gmail.com>
2020-05-29 19:00:46 +0900

    Just change something

--- neispy/__init__.py
+++ neispy/__init__.py
@@ -1,4 +1,8 @@
+from collections import namedtuple
+
 from .asynchttp import AsyncHttp
 from .asyncclient import AsyncClient
 from .synchttp import SyncHttp
 from .syncclient import SyncClient
+
+from .error import *
