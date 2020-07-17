6b33246f320684dc8577d5997170af161d18f09c
SaidBySolo <saidbysolo@gmail.com>
2020-07-18 01:06:19 +0900

    Update 2.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="neispy",
-    version="2.0.3",
+    version="2.0.4",
     license="MIT",
     author="Ryu ju heon",
     author_email="SaidBySolo@gmail.com",
