7affcb6f48fbd165c04558a97000f88987262a26
Ryu JuHeon <saidbysolo@gmail.com>
2020-09-10 23:17:15 +0900

    Update 3.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="neispy",
-    version="3.2.0",
+    version="3.2.1",
     license="MIT",
     author="Ryu ju heon",
     author_email="SaidBySolo@gmail.com",
